3f455fa98ef27de9c385395b686a2f04e44ba7da
Cedric Legarda <cedric_legarda28@yahoo.com>
2024-11-04 12:04:30 +0800

    chore: await for participation

--- src/services/participationService.ts
+++ src/services/participationService.ts
@@ -4,28 +4,28 @@ const prisma = new PrismaClient();
 
 export class ParticipationService {
     async createParticipation(data: { firstName: string; lastName: string; percentage: number; userId: number }) {
-        return prisma.participation.create({ data });
+        return await prisma.participation.create({ data });
     }
 
     async getAllParticipationsByUserId(userId: number) {
-        return prisma.participation.findMany({ where: { userId }, orderBy: {
+        return await prisma.participation.findMany({ where: { userId }, orderBy: {
             percentage: 'desc', // 'desc' for descending order
         } });
     }
 
     async getParticipationById(id: string) {
-        return prisma.participation.findUnique({ where: { id: parseInt(id) } });
+        return await prisma.participation.findUnique({ where: { id: parseInt(id) } });
     }
 
     async updateParticipation(id: string, data: { firstName: string; lastName: string; percentage: number }) {
-        return prisma.participation.update({
+        return await prisma.participation.update({
             where: { id: parseInt(id) },
             data,
         });
     }
 
     async deleteParticipation(id: string) {
-        return prisma.participation.delete({
+        return await prisma.participation.delete({
             where: { id: parseInt(id) },
         });
     }
